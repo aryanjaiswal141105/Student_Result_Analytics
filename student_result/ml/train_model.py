278b06afe6a38c3d1c3f9e94f5a288d923ceb744
aryanjaiswal141105 <aryan.jaiswal141105@gmail.com>
2026-07-09 11:55:40 +0530

    Pass Fail Prediction Model

--- student_result/ml/train_model.py
+++ student_result/ml/train_model.py
@@ -4,6 +4,12 @@ from sklearn.model_selection import train_test_split
 
 from model_utils import load_dataset
 
+from sklearn.tree import DecisionTreeClassifier
+
+from sklearn.metrics import accuracy_score
+
+import joblib
+
 dataset_path = r"C:\Users\Aryan\OneDrive\Desktop\Project\Data Analytics & ML\student_result\data\processed\student_analyzed.csv"
 
 
@@ -80,3 +86,73 @@ print("=" * 60)
 
 print("\nMachine Learning Dataset is Ready.")
 
+# Part 2 Begin 
+
+print("\n" + "=" * 60)
+print("STEP 14 : CREATE DECISION TREE MODEL")
+print("=" * 60)
+
+model = DecisionTreeClassifier(random_state=42)
+
+print("Decision Tree Model Created Successfully.")
+
+# Train these Model
+
+print("\n" + "=" * 60)
+print("STEP 15 : TRAIN MODEL")
+print("=" * 60)
+
+model.fit(X_train, y_train)
+
+print("Model Training Completed Successfully.")
+
+# Test the Model 
+
+print("\n" + "=" * 60)
+print("STEP 16 : TEST MODEL")
+print("=" * 60)
+
+y_pred = model.predict(X_test)
+
+print("Prediction Completed.")
+
+# Compare the Prediction
+
+print("\nActual Result")
+
+print(y_test.values)
+
+print("\nPredicted Result")
+
+print(y_pred)
+
+# Calculate the accuracy
+
+print("\n" + "=" * 60)
+print("STEP 17 : MODEL ACCURACY")
+print("=" * 60)
+
+accuracy = accuracy_score(y_test, y_pred)
+
+print(f"Accuracy : {accuracy * 100:.2f}%")
+
+print("\n" + "=" * 60)
+print("STEP 18 : SAVE MODEL")
+print("=" * 60)
+
+joblib.dump(
+    model,
+    "models/pass_fail_model.pkl"
+)
+
+print("Pass/Fail Model Saved Successfully.")
+
+# save the model
+
+print("\n" + "=" * 60)
+print("PASS / FAIL MODEL COMPLETED")
+print("=" * 60)
+
+print("Machine Learning Model is Ready.")
+
+# Final Message for prediciton 
