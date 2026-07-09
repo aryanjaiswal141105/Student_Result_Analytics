0046549c898ce485d6d9aaba3ef14465bb69fa60
aryanjaiswal141105 <aryan.jaiswal141105@gmail.com>
2026-07-09 11:32:54 +0530

    Dataset Preparation

--- student_result/ml/train_model.py
+++ student_result/ml/train_model.py
@@ -18,3 +18,65 @@ print(df.head())
 print("\nDataset Information")
 
 df.info()
+
+print("\n" + "=" * 60)
+print("STEP 11 : SELECT FEATURES")
+print("=" * 60)
+
+X = df[
+    [
+        "Maths",
+        "Science",
+        "English",
+        "Computer",
+        "Attendance"
+    ]
+]
+
+print("\nFeature Columns")
+
+print(X.head())
+
+print("\n" + "=" * 60)
+print("STEP 12 : SELECT TARGET")
+print("=" * 60)
+
+y = df["Result"]
+
+print("\nTarget Column")
+
+print(y.head())
+
+print("\n" + "=" * 60)
+print("STEP 13 : TRAIN TEST SPLIT")
+print("=" * 60)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.20,
+    random_state=42
+)
+
+print("\nTraining Feature Shape")
+
+print(X_train.shape)
+
+print("\nTesting Feature Shape")
+
+print(X_test.shape)
+
+print("\nTraining Target Shape")
+
+print(y_train.shape)
+
+print("\nTesting Target Shape")
+
+print(y_test.shape)
+
+print("\n" + "=" * 60)
+print("DATASET PREPARATION COMPLETED")
+print("=" * 60)
+
+print("\nMachine Learning Dataset is Ready.")
+
